dcf07cc3db8de70289a32be61a309de3958c21a5
richard <richard@ubuntu20.04>
2021-08-20 16:22:46 +0800

    disable goodix debug

--- drivers/input/touchscreen/goodix/gt9xx.h
+++ drivers/input/touchscreen/goodix/gt9xx.h
@@ -71,7 +71,7 @@
 
 #define GTP_GESTURE_WAKEUP    1    // gesture wakeup 
 
-#define GTP_DEBUG_ON          1
+#define GTP_DEBUG_ON          0
 #define GTP_DEBUG_ARRAY_ON    0
 #define GTP_DEBUG_FUNC_ON     0
 
